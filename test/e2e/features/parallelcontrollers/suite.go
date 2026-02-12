6d91bc3f3c2014ce4d60a25f83e52e37ef149927
David L. Chandler <david.chandler@solo.io>
2026-02-11 19:52:57 -0500

    rm TestParallelControllers
    Signed-off-by: David L. Chandler <david.chandler@solo.io>

--- test/e2e/features/parallelcontrollers/suite.go
+++
@@ -1,6 +0,0 @@
-//go:build e2e
-
-// Package parallelcontrollers previously contained tests for the dual controller
-// architecture (kgateway + agentgateway). Since agentgateway has been removed,
-// this package is no longer needed.
-package parallelcontrollers
