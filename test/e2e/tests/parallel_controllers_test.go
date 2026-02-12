b47a586cc38137ed1082f681cca5e05e71c89fc9
David L. Chandler <david.chandler@solo.io>
2026-02-11 19:54:28 -0500

    fixup rm TestParallelControllers
    Signed-off-by: David L. Chandler <david.chandler@solo.io>

--- test/e2e/tests/parallel_controllers_test.go
+++
@@ -1,6 +0,0 @@
-//go:build e2e
-
-// This test file previously tested the parallel controller architecture
-// (kgateway + agentgateway). Since agentgateway has been removed, these
-// tests are no longer needed.
-package tests_test
